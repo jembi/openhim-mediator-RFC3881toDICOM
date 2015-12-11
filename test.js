6fb003b2095fd648e45f78e8821b477684be7f3d
Ryan Crichton <rg.crichton@gmail.com>
2015-12-11 14:21:36 +0200

    Adds standalone test

--- test.js
+++ test.js
@@ -5,10 +5,10 @@ const mediator = require('./index');
 const net = require('net');
 const tap = require('tap');
 
-const xml = fs.readFileSync('rfc3881.xml', 'utf-8');
+const rfc3881 = fs.readFileSync('rfc3881.xml', 'utf-8');
 const dicom = fs.readFileSync('dicom.xml', 'utf-8');
 
-const audit =    `<85>1 2015-12-10T09:42:24.129Z ryan-Latitude-E6540 atna-audit.js 19381 IHE+RFC-3881 - ${xml}`;
+const audit =    `<85>1 2015-12-10T09:42:24.129Z ryan-Latitude-E6540 atna-audit.js 19381 IHE+RFC-3881 - ${rfc3881}`;
 const expected = `<85>1 2015-12-10T09:42:24.129Z ryan-Latitude-E6540 atna-audit.js 19381 IHE+RFC-3881 - ${dicom}`;
 
 tap.test('Main integration test', function(t) {
@@ -33,5 +33,15 @@ tap.test('Main integration test', function(t) {
         });
       });
     });
+  });
+});
+
+tap.test('Use as standalone lib', function(t) {
+  mediator.convertRFC3881toDICOM(rfc3881, (err, dicom) => {
+    if (err) {
+      return t.fail('Error returned', err);
+    }
+    t.equal(dicom, dicom);
+    t.end();
   });
 });
