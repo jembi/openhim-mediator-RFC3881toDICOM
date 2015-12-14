6ba484bf74ef37eae63b3905edf365388aed995e
Ryan Crichton <rg.crichton@gmail.com>
2015-12-14 17:06:28 +0200

    Add UPD server support
    Allows audit message to be received over UDP. Messages are then
    forwarded over TCP after that as with the TCP server.

--- test-server.js
+++ test-server.js
@@ -9,3 +9,21 @@ net.createServer((socket) => {
 }).listen(6262, () => {
   console.log('Test upstream server listening...');
 });
+
+const dgram = require('dgram');
+
+// test UDP server
+var server = dgram.createSocket('udp4');
+
+server.on('message', function (msg, rinfo) {
+  console.log('server got: ' + msg + ' from ' +
+    rinfo.address + ':' + rinfo.port);
+});
+
+server.on('listening', function () {
+  var address = server.address();
+  console.log('server listening ' +
+      address.address + ':' + address.port);
+});
+
+server.bind(6464);
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const dgram = require('dgram');
 const fs = require('fs');
 const mediator = require('./index');
 const net = require('net');
@@ -14,14 +15,13 @@ const dicom = fs.readFileSync('dicom.xml', 'utf-8');
 const audit =    `<85>1 2015-12-10T09:42:24.129Z ryan-Latitude-E6540 atna-audit.js 19381 IHE+RFC-3881 - ${rfc3881}`;
 const expected = `<85>1 2015-12-10T09:42:24.129Z ryan-Latitude-E6540 atna-audit.js 19381 IHE+RFC-3881 - ${dicom}`;
 
-tap.test('Main integration test', function(t) {
+tap.test('should convert audits received over TCP', function(t) {
   mediator.startMediator(() => {
     // start test upstream server
     let server = net.createServer((socket) => {
       console.log('connected to test upstream');
       socket.on('data', (buffer) => {
         t.equal(buffer.toString(), `${expected.length} ${expected}`);
-        t.end();
       });
     }).listen(6262, () => {
       console.log('Test upstream server listening...');
@@ -32,8 +32,33 @@ tap.test('Main integration test', function(t) {
           client.end();
           mediator.stopMediator(() => {
             server.close();
+            t.end();
           });
         });
+      });
+    });
+  });
+});
+
+tap.test('should convert audits received over UDP', function(t) {
+  mediator.startMediator(() => {
+    // start test upstream server
+    let server = net.createServer((socket) => {
+      console.log('connected to test upstream');
+      socket.on('data', (buffer) => {
+        t.equal(buffer.toString(), `${expected.length} ${expected}`);
+        mediator.stopMediator(() => {
+          server.close();
+          t.end();
+        });
+      });
+    }).listen(6262, () => {
+      console.log('Test upstream server listening...');
+      // send test rfc3881 message
+      let client = dgram.createSocket('udp4');
+      let buf = new Buffer(audit);
+      client.send(buf, 0, buf.length, 6363, 'localhost', () => {
+        client.close();
       });
     });
   });
